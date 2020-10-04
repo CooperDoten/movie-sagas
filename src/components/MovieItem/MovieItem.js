04624f0e74e5cf9b5a0d4befe752a107fa867daa
CooperDoten <cooper.doten@gmail.com>
2020-10-04 10:49:43 -0500

    added more styling to homeHeader in AddMovieButton

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -34,7 +34,7 @@ class MovieItem extends Component {
         <div className="movieItem"
               onClick={() => this.moreDetails(this.props.movie)}>
             <Link to='/moreDetails'>
-                <img src={this.props.movie.poster} alt="movie poster"/>
+                <img className="moviePoster" src={this.props.movie.poster} alt="movie poster"/>
             </Link>
         </div>
       </Router>
